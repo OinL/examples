5e59735e12d02fc770b068de5fb299e0e01cf4cd
OinL <l359584651@gmail.com>
2019-03-14 17:08:11 +0800

    2019/3/14

---
+++ vue.js/数字输入组件/input-number.js
@@ -0,0 +1,85 @@
+function isValueNumber(value) {
+    return (/(^-?[0-9]+\.{1}\d+$)|(^-?[1-9][0-9]*$)|(^-?0{1}$)/).test(value+'');
+}
+Vue.component('input-number',{
+    template:'\
+    <div class="input-number">\
+        <div class="input-number">\
+            <input \
+                type="text"\
+                :value="currentValue"\
+                @change="handleChange">\
+            <button \
+                @click="handleDown" \
+                :disabled="currentValue <= min">-</button> \
+            <button \
+                @click="handleUp"\
+                :disabled="currentValue >= max">+</button>\
+        </div>\
+    </div>',
+    props:{
+        max:{
+            type:Number,
+            default:Infinity   //正无穷
+        },
+        min:{
+            type:Number,
+            default:-Infinity  //负无穷
+        },
+        value:{
+            type:Number,
+            default: 0
+        }
+    },
+    data:function(){
+        return {
+            currentValue:this.value
+        }
+    },
+    watch:{
+        currentValue:function(val){
+            this.$emit('input',val);
+            this.$emit('on-change',val);
+        },
+        value:function(val){
+            this.updateValue(val);
+        }
+    },
+    methods:{
+        handleDown:function(){
+            if(this.currentValue <= this.min) return;
+            this.currentValue -= 1;
+        },
+        handleUp:function(){
+            if(this.currentValue >= this.max) return;
+            this.currentValue += 1;
+        },
+        handleChange:function(event){
+            var val = event.target.value.trim();
+
+            var max = this.max;
+            var min = this.min;
+
+            if(isValueNumber(val)){
+                val = Number(val);
+                this.currentValue = val;
+
+                if(val>max){
+                    this.currentValue = max;
+                }else if(val < min){
+                    this.currentValue = min;
+                }
+            }else{
+                event.target.value = this.currentValue;
+            }
+        },
+        updateValue:function(val){
+            if(val>this.max) val=this.max;
+            if(val<this.min) val=this.min;
+            this.currentValue=val;
+        }
+    },
+    mounted:function(){
+        this.updateValue(this.value);
+    }
+})
